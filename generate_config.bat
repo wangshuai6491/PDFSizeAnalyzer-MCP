ccc00b2bcd478789d4d6771bd2ae792ddd1eb43a
wangshuai6491 <649107757@qq.com>
2025-05-19 17:32:43 +0800

    修改

--- generate_config.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "venvPath=%~dp0venv\Scripts\python.exe"
-set "mainPath=%~dp0main.py"
-set "jsonContent={\"mcpServers\":{\"PDFAnalyzer\":{\"command\":\"%venvPath%\",\"args\":[\"%mainPath%\"]}}}"
-(echo %jsonContent%) > mcp_config.json
